a72d70b621e760b0a15fb875d15a93529943ecd3
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-07-16 23:45:10 +0200

    update task model

--- lib/model/task_model.dart
+++ lib/model/task_model.dart
@@ -3,6 +3,15 @@ class Task {
   final String description;
   final bool notificable;
   final bool done;
+  final String time;
+  final bool currentTime;
 
-  const Task({this.title, this.description, this.notificable, this.done});
+  const Task(
+      {this.title,
+      this.description,
+      this.notificable,
+      this.done,
+      this.time,
+      currentTime})
+      : currentTime = currentTime ?? false;
 }
