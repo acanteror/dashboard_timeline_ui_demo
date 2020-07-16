4924e8a454dce30208e5e4e737096e45106931af
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-07-16 20:38:30 +0200

    add fake data

---
+++ lib/data/fake.dart
@@ -0,0 +1,189 @@
+import 'package:dashboard_timeline_ui_demo/model/day_model.dart';
+import 'package:dashboard_timeline_ui_demo/model/task_model.dart';
+
+List<Day> data = <Day>[
+  Day(monthNumber: 1, weekDay: 'MON', tasks: [
+    Task(
+      title: 'Morning run in the park',
+      description: 'Mon - Fri',
+      done: true,
+      notificable: false,
+    ),
+    Task(
+      title: 'Skype meeting with the NY contractor',
+      description: 'Delivery overview',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Meeting with the design team',
+      description: 'Project objectives',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Weekly Meeting with the team',
+      description: 'On the list of issues',
+      done: false,
+      notificable: true,
+    )
+  ]),
+  Day(monthNumber: 2, weekDay: 'TUE', tasks: [
+    Task(
+      title: 'Morning run in the park',
+      description: 'Mon - Fri',
+      done: true,
+      notificable: false,
+    ),
+    Task(
+      title: 'Skype meeting with the NY contractor',
+      description: 'Delivery overview',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Meeting with the design team',
+      description: 'Project objectives',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Weekly Meeting with the team',
+      description: 'On the list of issues',
+      done: false,
+      notificable: true,
+    )
+  ]),
+  Day(monthNumber: 3, weekDay: 'WED', tasks: [
+    Task(
+      title: 'Morning run in the park',
+      description: 'Mon - Fri',
+      done: true,
+      notificable: false,
+    ),
+    Task(
+      title: 'Skype meeting with the NY contractor',
+      description: 'Delivery overview',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Meeting with the design team',
+      description: 'Project objectives',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Weekly Meeting with the team',
+      description: 'On the list of issues',
+      done: false,
+      notificable: true,
+    )
+  ]),
+  Day(monthNumber: 4, weekDay: 'THU', tasks: [
+    Task(
+      title: 'Morning run in the park',
+      description: 'Mon - Fri',
+      done: true,
+      notificable: false,
+    ),
+    Task(
+      title: 'Skype meeting with the NY contractor',
+      description: 'Delivery overview',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Meeting with the design team',
+      description: 'Project objectives',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Weekly Meeting with the team',
+      description: 'On the list of issues',
+      done: false,
+      notificable: true,
+    )
+  ]),
+  Day(monthNumber: 5, weekDay: 'FRI', tasks: [
+    Task(
+      title: 'Morning run in the park',
+      description: 'Mon - Fri',
+      done: true,
+      notificable: false,
+    ),
+    Task(
+      title: 'Skype meeting with the NY contractor',
+      description: 'Delivery overview',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Meeting with the design team',
+      description: 'Project objectives',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Weekly Meeting with the team',
+      description: 'On the list of issues',
+      done: false,
+      notificable: true,
+    )
+  ]),
+  Day(monthNumber: 6, weekDay: 'SAT', tasks: []),
+  Day(monthNumber: 7, weekDay: 'SUN', tasks: []),
+  Day(monthNumber: 8, weekDay: 'MON', tasks: [
+    Task(
+      title: 'Morning run in the park',
+      description: 'Mon - Fri',
+      done: true,
+      notificable: false,
+    ),
+    Task(
+      title: 'Skype meeting with the NY contractor',
+      description: 'Delivery overview',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Meeting with the design team',
+      description: 'Project objectives',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Weekly Meeting with the team',
+      description: 'On the list of issues',
+      done: false,
+      notificable: true,
+    )
+  ]),
+  Day(monthNumber: 9, weekDay: 'TUE', tasks: [
+    Task(
+      title: 'Morning run in the park',
+      description: 'Mon - Fri',
+      done: true,
+      notificable: false,
+    ),
+    Task(
+      title: 'Skype meeting with the NY contractor',
+      description: 'Delivery overview',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Meeting with the design team',
+      description: 'Project objectives',
+      done: false,
+      notificable: true,
+    ),
+    Task(
+      title: 'Weekly Meeting with the team',
+      description: 'On the list of issues',
+      done: false,
+      notificable: true,
+    )
+  ]),
+];
