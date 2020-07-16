27207a43da33d3ebd11b68ebd080d005c3cc8a06
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-07-16 22:47:01 +0200

    move file

---
+++ lib/widget/shared/day_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:dashboard_timeline_ui_demo/model/day_model.dart';
+import 'package:dashboard_timeline_ui_demo/extension/context_extension.dart';
+
+typedef void OnPress();
+
+class DayCard extends StatelessWidget {
+  const DayCard({
+    Key key,
+    @required this.day,
+    this.onPress,
+  }) : super(key: key);
+
+  final Day day;
+  final OnPress onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    final fontColor = day.currentDay ? Colors.white : Colors.black;
+    final backColor = day.currentDay ? Colors.orange : Colors.white;
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        decoration: BoxDecoration(
+            color: backColor,
+            border: Border.all(color: backColor),
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            )),
+        margin: EdgeInsets.all(context.pch(.5)),
+        padding: EdgeInsets.all(context.pch(.5)),
+        width: context.pcw(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              day.monthNumber.toString(),
+              style: TextStyle(
+                  color: fontColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: context.pcw(6)),
+            ),
+            Text(
+              day.weekDay.toUpperCase(),
+              style: TextStyle(
+                  color: fontColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: context.pcw(2.8)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
