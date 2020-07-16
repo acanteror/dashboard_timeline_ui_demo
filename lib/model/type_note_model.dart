ce86e59e6c3d8e88a76ca8f0b9d58616a1a56776
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-07-16 22:00:06 +0200

    add type_note_model

---
+++ lib/model/type_note_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TypeNote {
+  final IconData iconData;
+  final String title;
+  final int items;
+
+  const TypeNote({
+    this.iconData,
+    this.title,
+    this.items,
+  });
+}
