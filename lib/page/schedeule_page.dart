2ab474057f9f9abd872138d5c81ad3d98f63b0e5
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-07-16 23:12:18 +0200

    add schedeule_ist

--- lib/page/schedeule_page.dart
+++ lib/page/schedeule_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:dashboard_timeline_ui_demo/widget/schedeule/schedeule_list.dart';
 import 'package:dashboard_timeline_ui_demo/widget/schedeule/schedeule_header.dart';
 import 'package:dashboard_timeline_ui_demo/model/day_model.dart';
 import 'package:dashboard_timeline_ui_demo/page/base_page.dart';
 
 class SchedeulePage extends StatelessWidget {
-  SchedeulePage({Key key, this.day}) : super(key: key);
+  SchedeulePage({
+    Key key,
+    this.day,
+  }) : super(key: key);
 
   final Day day;
 
@@ -13,9 +17,7 @@ class SchedeulePage extends StatelessWidget {
     return BasePage(
       title: 'Schedeule',
       childTop: SchedeuleHeader(day: day),
-      childBody: Center(
-        child: Text('pong!!!!'),
-      ),
+      childBody: SchedeuleList(tasks: day.tasks),
     );
   }
 }
