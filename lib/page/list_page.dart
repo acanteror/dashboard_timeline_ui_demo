a71155726b910fe31d531c71a096c4355f6fb8af
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-07-16 22:48:00 +0200

    remove title param

--- lib/page/list_page.dart
+++ lib/page/list_page.dart
@@ -4,15 +4,14 @@ import 'package:dashboard_timeline_ui_demo/widget/list/type_note_grid.dart';
 import 'package:dashboard_timeline_ui_demo/page/base_page.dart';
 
 class ListPage extends StatelessWidget {
-  ListPage({Key key, this.title}) : super(key: key);
-  final String title;
+  ListPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BasePage(
       title: 'My List',
       childTop: DayList(),
-      child2: TypeNoteGrid(),
+      childBody: TypeNoteGrid(),
     );
   }
 }
