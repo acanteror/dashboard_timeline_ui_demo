7ed5fb5b0f82d2a87da94e5aebb09e522d8a2faa
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-07-16 22:47:51 +0200

    add schedeule header

--- lib/page/schedeule_page.dart
+++ lib/page/schedeule_page.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:dashboard_timeline_ui_demo/widget/schedeule/schedeule_header.dart';
+import 'package:dashboard_timeline_ui_demo/model/day_model.dart';
 import 'package:dashboard_timeline_ui_demo/page/base_page.dart';
-import 'package:dashboard_timeline_ui_demo/extension/context_extension.dart';
 
 class SchedeulePage extends StatelessWidget {
-  SchedeulePage({Key key, this.title}) : super(key: key);
-  final String title;
+  SchedeulePage({Key key, this.day}) : super(key: key);
+
+  final Day day;
 
   @override
   Widget build(BuildContext context) {
     return BasePage(
       title: 'Schedeule',
-      childTop: Container(
-          padding: EdgeInsets.all(context.pch(.5)),
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[Container(color: Colors.white)],
-          )),
-      child2: Center(
-        child: Text('ping!!!!'),
+      childTop: SchedeuleHeader(day: day),
+      childBody: Center(
+        child: Text('pong!!!!'),
       ),
     );
   }
