f1b946bda3021db44a42335d0e65b694da33858e
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-07-16 21:15:57 +0200

    add conditional colors

--- lib/widget/day_card.dart
+++ lib/widget/day_card.dart
@@ -1,3 +1,4 @@
+import 'package:dashboard_timeline_ui_demo/data/fake.dart';
 import 'package:flutter/material.dart';
 import 'package:dashboard_timeline_ui_demo/model/day_model.dart';
 import 'package:dashboard_timeline_ui_demo/extension/context_extension.dart';
@@ -12,10 +13,13 @@ class DayCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(day.currentDay);
+    final fontColor = day.currentDay ? Colors.white : Colors.black;
+    final backColor = day.currentDay ? Colors.orange : Colors.white;
     return Container(
       decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.white),
+          color: backColor,
+          border: Border.all(color: backColor),
           borderRadius: BorderRadius.all(
             Radius.circular(5),
           )),
@@ -28,12 +32,16 @@ class DayCard extends StatelessWidget {
           Text(
             day.monthNumber.toString(),
             style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: context.pcw(6)),
+                color: fontColor,
+                fontWeight: FontWeight.bold,
+                fontSize: context.pcw(6)),
           ),
           Text(
             day.weekDay.toUpperCase(),
             style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: context.pcw(2.8)),
+                color: fontColor,
+                fontWeight: FontWeight.bold,
+                fontSize: context.pcw(2.8)),
           ),
         ],
       ),
