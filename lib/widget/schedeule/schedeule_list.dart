2b161d2a357976b28c037593fe0a8cba57e62ca0
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-07-16 23:12:07 +0200

    extract schedeule_list

---
+++ lib/widget/schedeule/schedeule_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:dashboard_timeline_ui_demo/model/task_model.dart';
+import 'package:dashboard_timeline_ui_demo/widget/schedeule/timeline_card.dart';
+
+class SchedeuleList extends StatelessWidget {
+  const SchedeuleList({
+    Key key,
+    @required this.tasks,
+  }) : super(key: key);
+
+  final List<Task> tasks;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        final _task = tasks[index];
+        return TimelineCard(task: _task);
+      },
+    );
+  }
+}
